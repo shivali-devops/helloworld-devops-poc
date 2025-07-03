e7b3ddc57c87cbc4571185e447c2a1229e95d4f9
shivali-devops <shivalikakade01@gmail.com>
2025-07-03 14:51:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-  return "Hello from Devops Pipeline!"
+  return "Hello from Devops Pipeline 3rd July!"
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0',port=8080)
