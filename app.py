eaad9c6463f04c14a42344fef941b186d34a86e3
shivali-devops <shivalikakade01@gmail.com>
2025-07-06 14:16:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-  return "Hello from Devops Pipeline 3rd July!"
+def home():
+    return render_template("index.html")
 
-if __name__ == "__main__":
-  app.run(host='0.0.0.0',port=8080)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
