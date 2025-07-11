71d314404df8304e7d5c6cee210b21b6e7be1e1c
shivali-devops <shivalikakade01@gmail.com>
2025-07-11 12:36:49 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,6 +1,13 @@
 # test__app.py
 
+#import app
+
+#def test_import_only():
+    #assert app.app is not None
+
 import app
 
-def test_import_only():
-    assert app.app is not None
+def test_home():
+    response = app.app.test_client().get('/')
+    assert response.status_code == 200
+    assert b"Hello" in response.data
